9889370303187602761119598eabb7776645aec2
Guilherme Santana <guilhermesanttana2016@gmail.com>
2026-08-26 08:13:51 -0300

    Add files via upload

---
+++ Python/Exercicios_Guanabara/ex020.py
@@ -0,0 +1,10 @@
+import random
+
+aluno1 = input("Digite o nome do primeiro aluno: ")
+aluno2 = input("Digite o nome do segundo aluno: ")
+aluno3 = input("Digite o nome do terceiro aluno: ")
+aluno4 = input("Digite o nome do quarto aluno: ")
+participantes = [aluno1, aluno2, aluno3, aluno4]
+
+random.shuffle(participantes)
+print(f"Os alunos escolhidos foram: {participantes}")
---
+++ Python/Exercicios_Guanabara/ex022.py
@@ -0,0 +1,4 @@
+nome = input("Digite um nome: ")
+print(len(nome))
+print(nome.upper())
+print(nome.lower())
